0262dbe1a0934e2a84dd3c968742032e5a5314b6
Robert Beene <robert@ismly.com>
2024-09-27 12:02:44 -0400

    Base configs for Rails

---
+++ .config/nvim/init.lua
@@ -0,0 +1,26 @@
+require("config.lazy")
+
+local vim = vim
+
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set number")
+
+local run_test = function()
+  local current_file = vim.fn.expand('%:p')
+  local test_string = "silent !zellij run -d Down -n 'testing' -- bin/rails test " .. current_file
+  vim.api.nvim_command(test_string)
+end
+
+local run_all_tests = function()
+  local test_string = "silent !zellij run -d Down -n 'testing' -- bin/rails test"
+  vim.api.nvim_command(test_string)
+end
+
+vim.api.nvim_create_user_command('RunTest', run_test, {})
+vim.api.nvim_create_user_command('RunAllTests', run_all_tests, {})
+
+vim.keymap.set("n", "<leader>t", ":RunTest<CR>", { desc = "run current file test" })
+vim.keymap.set("n", "<leader>a", ":RunAllTests<CR>", { desc = "run all tests" })
---
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,43 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls"},
+        automatic_installation = true,
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+
+      local lspconfig = require('lspconfig')
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities
+      })
+      lspconfig.ruby_lsp.setup({
+        init_options = {
+          formatter = 'standard',
+          linters = { 'standard' },
+        },
+        capabilities = capabilities,
+        cmd = { vim.fn.expand "/usr/local/bundle/bin/ruby-lsp"},
+      })
+    end,
+
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = "hover documentation LSP" }),
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = "Code Action LSP" }),
+    vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, { desc = "GOTO Definition LSP" }),
+    vim.keymap.set('n', '<leader>gr', vim.lsp.buf.references, { desc = "GOTO References LSP" }),
+    vim.keymap.set('n', '<leader>gf', vim.lsp.buf.format, { desc = "Format File LSP" }),
+  }
+}
